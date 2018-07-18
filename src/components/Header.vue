57c91c60ecfe6d6de37dd1257bb0829a9116583f
Tasuja <sujata@vayu.com.au>
2018-07-18 16:56:28 +0545

    changes made to save data method

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -30,7 +30,7 @@
           <ul 
               class="dropdown-menu" 
               >
-            <li><a href="#">Save Data</a></li>
+            <li><a href="#" @click="saveData">Save Data</a></li>
             <li><a href="#">Load Data</a></li>
             <li><a href="#">Separated link</a></li>
           </ul>
@@ -60,6 +60,14 @@
         ]),
         endDay() {
           this.randomizeStocks();
+        },
+        saveData() {
+        const data = {
+            funds: this.$store.getters.funds,
+            stockPortfolio: this.$store.getters.stockPortfolio,
+            stocks: this.$store.getters.stocks
+        };
+        this.$http.put('data.json', data);
         }
     }
   }
